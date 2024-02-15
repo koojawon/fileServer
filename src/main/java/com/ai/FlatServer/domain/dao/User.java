3c80b236ae65cca1170309d3aaa363097d5016a6
koojawon <jawonkoo@naver.com>
2024-02-15 14:10:42 +0900

    -feat: user entity 생성

---
+++ src/main/java/com/ai/FlatServer/domain/dao/User.java
@@ -0,0 +1,58 @@
+package com.ai.FlatServer.domain.dao;
+
+import com.ai.FlatServer.enums.Role;
+import com.ai.FlatServer.enums.SocialType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "Users")
+@AllArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    private String socialId;
+
+    private String refreshToken;
+
+    public void authorizeUser() {
+        this.role = Role.USER;
+    }
+
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateRefreshToken(String newToken) {
+        this.refreshToken = newToken;
+    }
+}
