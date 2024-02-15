eba2e9b1ee74fb3cf7f2d91f64c1047d535285ac
koojawon <jawonkoo@naver.com>
2024-02-15 14:10:57 +0900

    -feat: user repository 생성

---
+++ src/main/java/com/ai/FlatServer/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.ai.FlatServer.repository;
+
+import com.ai.FlatServer.domain.dao.User;
+import com.ai.FlatServer.enums.SocialType;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
+
+    Optional<User> findByRefreshToken(String refreshToken);
+
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+}
