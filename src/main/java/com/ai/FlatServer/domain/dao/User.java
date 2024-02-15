15e7b16823a88d5fd38e505e669d8ef48ae37df4
koojawon <jawonkoo@naver.com>
2024-02-15 14:41:28 +0900

    -refactor: security 추가로 인한 class import

--- src/main/java/com/ai/FlatServer/domain/dao/User.java
+++ src/main/java/com/ai/FlatServer/domain/dao/User.java
@@ -15,6 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
