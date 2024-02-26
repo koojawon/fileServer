abe6aac094161b89d948780328dc13c9fdc30fdc
koojawon <jawonkoo@naver.com>
2024-02-26 14:21:22 +0900

    -feat 폴더 초안 작성

---
+++ src/main/java/com/ai/FlatServer/domain/dao/FolderDao.java
@@ -0,0 +1,38 @@
+package com.ai.FlatServer.domain.dao;
+
+import com.ai.FlatServer.repository.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@RequiredArgsConstructor
+@Builder
+public class FolderDao extends BaseEntity {
+
+    @Id
+    @Column(updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long id;
+
+    @Column(nullable = false)
+    private final String name;
+
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private FolderDao parent;
+
+    @OneToOne
+    @JoinColumn(name = "owner_id")
+    private User user;
+}
