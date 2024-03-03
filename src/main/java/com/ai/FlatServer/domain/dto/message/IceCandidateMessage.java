0934aaabe35947a5183f6a8889397ce47be670e6
koojawon <jawonkoo@naver.com>
2024-03-03 13:31:24 +0900

    -feat 개인 구별용 uuid 사용

--- src/main/java/com/ai/FlatServer/domain/dto/message/IceCandidateMessage.java
+++ src/main/java/com/ai/FlatServer/domain/dto/message/IceCandidateMessage.java
@@ -1,5 +1,6 @@
 package com.ai.FlatServer.domain.dto.message;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -7,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Data
 @Builder
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class IceCandidateMessage {
     private String id;
+    private String uuid;
     private String candidate;
     private String sdpMid;
     private int sdpMLineIndex;
