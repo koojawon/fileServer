7fee0c9a0dd6ba34159c05deedddefd6aacfe12f
koojawon <jawonkoo@naver.com>
2024-03-03 13:35:20 +0900

    -feat 개인 식별용 uuid, icecandidate 임시 저장기능 생성

--- src/main/java/com/ai/FlatServer/domain/session/UserSession.java
+++ src/main/java/com/ai/FlatServer/domain/session/UserSession.java
@@ -2,6 +2,8 @@ package com.ai.FlatServer.domain.session;
 
 import com.google.gson.JsonObject;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,20 +20,31 @@ import org.springframework.web.socket.WebSocketSession;
 @AllArgsConstructor
 @Builder
 public class UserSession {
+    private final List<IceCandidate> candidateList = new ArrayList<>();
     private WebSocketSession session;
+    private String uuid;
     private WebRtcEndpoint webRtcEndpoint;
+    private String sdpOffer;
 
     public void sendMessage(JsonObject message) throws IOException {
         log.debug("Sending message from user '{}' : {}", session.getId(), message);
         session.sendMessage(new TextMessage(message.toString()));
     }
 
+    public void setWebRtcEndpoint(WebRtcEndpoint webRtcEndpoint) {
+        this.webRtcEndpoint = webRtcEndpoint;
+
+        for (IceCandidate e : candidateList) {
+            this.webRtcEndpoint.addIceCandidate(e);
+        }
+        this.candidateList.clear();
+    }
+
     public void addCandidate(IceCandidate candidate) {
-        webRtcEndpoint.addIceCandidate(candidate);
+        if (this.webRtcEndpoint != null) {
+            webRtcEndpoint.addIceCandidate(candidate);
+            return;
+        }
+        candidateList.add(candidate);
     }
-
-    public boolean isEmptySession() {
-        return session == null && webRtcEndpoint == null;
-    }
-
 }
