53342eb52a4362d63a344d0da696099493600c1d
koojawon <jawonkoo@naver.com>
2024-03-18 12:45:56 +0900

    -refactor 기존 handler를 controller/service로 분리

---
+++ src/main/java/com/ai/FlatServer/controller/webrtc/ViewerController.java
@@ -0,0 +1,56 @@
+package com.ai.FlatServer.controller.webrtc;
+
+import com.ai.FlatServer.domain.dto.message.TargetInfoResponseMessage;
+import com.ai.FlatServer.domain.mapper.JsonMessageDecoder;
+import com.ai.FlatServer.service.webrtc.MediaPipelineService;
+import com.ai.FlatServer.service.webrtc.PresenterService;
+import com.ai.FlatServer.service.webrtc.ViewerService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ViewerController {
+
+    private final JsonMessageDecoder decoder;
+
+    private final ViewerService viewerService;
+
+    private final PresenterService presenterService;
+
+    private final MediaPipelineService mediaPipelineService;
+
+    @RabbitListener(queues = "pdfJobQueue.senderQueue")
+    public void handleMessage(String message) throws IOException {
+        JsonObject jsonObject = JsonParser.parseString(message).getAsJsonObject();
+        switch (jsonObject.get("id").getAsString()) {
+            case "viewer" -> handleViewerMessage(jsonObject);
+            case "onIceCandidate" -> viewerService.onIceCandidate(decoder.toIceCandidateMessage(jsonObject));
+        }
+    }
+
+
+    private void handleViewerMessage(JsonObject jsonObject) throws IOException {
+        TargetInfoResponseMessage message = decoder.toTargetInfoResponseMessage(jsonObject);
+        String viewerId = viewerService.initViewer(message);
+
+        mediaPipelineService.createMediaPipeline(message.getTargetId());
+        mediaPipelineService.enableStatsOfId(message.getTargetId());
+        String presenterId = presenterService.initPresenter(message.getTargetId());
+        mediaPipelineService.connectEach(presenterId, viewerId);
+
+        presenterService.processOffer(message.getTargetId());
+        viewerService.processOffer(viewerId);
+
+        viewerService.startGathering(viewerId);
+        presenterService.startGathering(presenterId);
+
+        mediaPipelineService.setRelation(presenterId, viewerId);
+    }
+}
---
+++ src/main/java/com/ai/FlatServer/service/webrtc/ViewerService.java
@@ -0,0 +1,97 @@
+package com.ai.FlatServer.service.webrtc;
+
+import com.ai.FlatServer.domain.dto.message.IceCandidateMessage;
+import com.ai.FlatServer.domain.dto.message.TargetInfoResponseMessage;
+import com.ai.FlatServer.domain.mapper.JsonMessageEncoder;
+import com.ai.FlatServer.domain.session.UserSession;
+import com.ai.FlatServer.repository.ClientRepository;
+import com.ai.FlatServer.service.MessageService;
+import com.google.gson.JsonObject;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kurento.client.IceCandidate;
+import org.kurento.client.WebRtcEndpoint;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketSession;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ViewerService {
+
+    private final JsonMessageEncoder encoder;
+
+    private final MessageService messageService;
+    private final ClientRepository clientRepository;
+
+    public void sendTargetInfo(String targetId, JsonObject jsonMessage) {
+        String fileName = jsonMessage.get("fileId").getAsString();
+        JsonObject jsonObject = encoder.toTargetInfoMessage(targetId, fileName);
+        messageService.sendMessage(jsonObject);
+    }
+
+    public void notifyEnd(WebSocketSession session) {
+        String uuid = clientRepository.getViewerWithPresenter(session.getId());
+        if (uuid != null) {
+            JsonObject jsonObject = encoder.toEndMessage(session.getId());
+            messageService.sendMessage(jsonObject);
+            clientRepository.removeUser(uuid);
+        }
+    }
+
+    public void onIceCandidate(IceCandidateMessage message) {
+        IceCandidate candidate = new IceCandidate(message.getCandidate(), message.getSdpMid(),
+                message.getSdpMLineIndex());
+        clientRepository.getUser(message.getUuid()).addCandidate(candidate);
+    }
+
+    public void reject() {
+        JsonObject jsonObject = encoder.toRejectMessage();
+        messageService.sendMessage(jsonObject);
+    }
+
+    public String initViewer(TargetInfoResponseMessage message) {
+        if (clientRepository.getUser(message.getTargetId()) == null) {
+            reject();
+            throw new NoSuchElementException("No such presenter exists!");
+        } else {
+            UserSession viewerSession = UserSession.builder().uuid(message.getUuid()).build();
+            viewerSession.setSdpOffer(message.getSdpOffer());
+            clientRepository.putUser(viewerSession.getUuid(), viewerSession);
+
+            WebRtcEndpoint viewerWebRtc = createViewerEndpoint(message.getTargetId());
+            viewerSession.setWebRtcEndpoint(viewerWebRtc);
+
+            return viewerSession.getUuid();
+        }
+    }
+
+    private WebRtcEndpoint createViewerEndpoint(String id) {
+        WebRtcEndpoint viewerWebRtc = new WebRtcEndpoint.Builder(
+                clientRepository.getMediaPipelineBySessionId(id)).useDataChannels().build();
+
+        viewerWebRtc.addIceCandidateFoundListener(event -> {
+            JsonObject jsonObject = encoder.toIceCandidateMessage(event.getCandidate());
+            try {
+                messageService.sendMessage(jsonObject);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        });
+        return viewerWebRtc;
+    }
+
+    public void processOffer(String id) {
+        UserSession viewer = clientRepository.getUser(id);
+        WebRtcEndpoint viewerEndpoint = viewer.getWebRtcEndpoint();
+        String answer = viewerEndpoint.processOffer(viewer.getSdpOffer());
+        JsonObject jsonObject = encoder.toViewerResponse(answer);
+        messageService.sendMessage(jsonObject);
+    }
+
+    public void startGathering(String viewerId) {
+        UserSession viewer = clientRepository.getUser(viewerId);
+        viewer.getWebRtcEndpoint().gatherCandidates();
+    }
+}
