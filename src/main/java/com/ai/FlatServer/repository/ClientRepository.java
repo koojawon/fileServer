1a67c88de18fc4f78727e8b03bc1a3444a41d717
koojawon <jawonkoo@naver.com>
2024-03-18 12:42:11 +0900

    -refactor viewer/presenter 통합, 누락된 함수 추가

--- src/main/java/com/ai/FlatServer/repository/ClientRepository.java
+++ src/main/java/com/ai/FlatServer/repository/ClientRepository.java
@@ -2,23 +2,25 @@ package com.ai.FlatServer.repository;
 
 import com.ai.FlatServer.domain.session.UserSession;
 import org.kurento.client.MediaPipeline;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository {
     void setListenRelation(String presenterSessionId, String viewerUuid);
 
-    void removeViewer(String id);
+    void removeListenRelation(String sessionId);
+
+    void putUser(String id, UserSession session);
+
+    UserSession getUser(String id);
+
+    void removeUser(String id);
+
+    String getViewerWithPresenter(String sessionId);
 
     MediaPipeline getMediaPipelineBySessionId(String id);
 
     void putMediaPipelineBySessionId(String id, MediaPipeline pipeline);
 
-    void putPresenter(String id, UserSession userSession);
-
-    void removePresenter(String id);
-
     void removeMediaPipelineBySessionId(String id);
-
-    UserSession getPresenter(String id);
-
-    void removeListenRelation(String sessionId);
 }
