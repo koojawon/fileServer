b51e804ad8c31f22ffef587a33d30c0183a38637
koojawon <jawonkoo@naver.com>
2024-03-25 12:22:23 +0900

    -feat 폴더 레포지토리 생성

---
+++ src/main/java/com/ai/FlatServer/repository/FolderRepository.java
@@ -0,0 +1,9 @@
+package com.ai.FlatServer.repository;
+
+import com.ai.FlatServer.repository.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
