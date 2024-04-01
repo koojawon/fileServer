7a206385081604bb88636cac59aa3a6238089a0b
koojawon <jawonkoo@naver.com>
2024-04-01 12:18:46 +0900

    -feat 하위 폴더/파일 삭제 구현

--- src/main/java/com/ai/FlatServer/file/respository/FileInfoRepository.java
+++ src/main/java/com/ai/FlatServer/file/respository/FileInfoRepository.java
@@ -1,9 +1,11 @@
 package com.ai.FlatServer.file.respository;
 
 import com.ai.FlatServer.file.respository.dao.FileInfo;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,5 +13,11 @@ public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
 
     Optional<FileInfo> findByUid(String uid);
 
-    ArrayList<FileInfo> findAllByFav(boolean bool);
+    List<FileInfo> findAllByFav(boolean bool);
+
+    List<FileInfo> findAllByParentFolderId(Long id);
+
+    @Modifying
+    @Query("delete from FileInfo f where f.parentFolderId in :ids")
+    void deleteAllByParentFolderId(List<Long> ids);
 }
