b3691131de8c277871c59fda072042972207b934
koojawon <jawonkoo@naver.com>
2024-04-01 12:24:09 +0900

    -feat 캐싱 적용/ 매핑 방식 변경/ 하위 폴더/파일 삭제 구현

--- src/main/java/com/ai/FlatServer/folder/service/FolderService.java
+++ src/main/java/com/ai/FlatServer/folder/service/FolderService.java
@@ -1,16 +1,25 @@
 package com.ai.FlatServer.folder.service;
 
-import com.ai.FlatServer.folder.dto.response.FolderInfo;
-import com.ai.FlatServer.folder.dto.request.FolderPatchRequest;
-import com.ai.FlatServer.folder.dto.mapper.FolderMapper;
 import com.ai.FlatServer.file.respository.FileInfoRepository;
+import com.ai.FlatServer.file.respository.dao.FileInfo;
+import com.ai.FlatServer.folder.dto.mapper.FolderMapper;
+import com.ai.FlatServer.folder.dto.request.FolderPatchRequest;
+import com.ai.FlatServer.folder.dto.response.FolderInfo;
 import com.ai.FlatServer.folder.repository.FolderRepository;
 import com.ai.FlatServer.folder.repository.entity.Folder;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Queue;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +29,8 @@ public class FolderService {
 
     private final FolderRepository folderRepository;
     private final FileInfoRepository fileInfoRepository;
+
+    private final CacheManager cacheManager;
 
     @PostConstruct
     @Transactional
@@ -32,12 +43,15 @@ public class FolderService {
         }
     }
 
+    @Cacheable(value = "folderCache", key = "#folderId")
     public FolderInfo getFolderWithId(Long folderId) {
         Folder folder = folderRepository.findById(folderId).orElseThrow(NoSuchElementException::new);
-        return FolderMapper.FolderToFolderInfoMapper(folder);
+        List<FileInfo> folderInfos = fileInfoRepository.findAllByParentFolderId(folder.getId());
+        return FolderMapper.FolderToFolderInfoMapper(folder, folderInfos);
     }
 
     @Transactional
+    @CacheEvict(value = "folderCache", key = "#currentFolderId")
     public void createFolderAt(String folderName, Long currentFolderId) {
         Folder surFolder = folderRepository.findById(currentFolderId).orElseThrow(NoSuchElementException::new);
         Folder folder = Folder.builder().folderName(folderName).parent(surFolder).build();
@@ -46,15 +60,39 @@ public class FolderService {
     }
 
     @Transactional
-    public void deleteFolder(Long currentFolderId) {
-        Folder folder = folderRepository.findById(currentFolderId).orElseThrow(NoSuchElementException::new);
-        fileInfoRepository.deleteAll(folder.getSubFiles());
-        folderRepository.delete(folder);
+    @CacheEvict(value = "fileCache", key = "'all'")
+    public void deleteFolder(Long targetFolderId) {
+        Folder folder = folderRepository.findById(targetFolderId).orElseThrow(NoSuchElementException::new);
+
+        List<Long> folderIds = searchSubFolderIds(folder);
+        fileInfoRepository.deleteAllByParentFolderId(folderIds);
+        folderRepository.deleteAllByIds(folderIds);
+
+        Objects.requireNonNull(cacheManager.getCache("folderCache")).evict(folder.getParent().getId());
+        for (Long id : folderIds) {
+            Objects.requireNonNull(cacheManager.getCache("folderCache")).evict(id);
+        }
+    }
+
+    private List<Long> searchSubFolderIds(Folder folder) {
+        List<Long> rval = new ArrayList<>();
+        Queue<Folder> q = new LinkedList<>(folder.getSubDirs());
+        while (!q.isEmpty()) {
+            Folder cur = q.poll();
+            rval.add(cur.getId());
+            for (Folder f : cur.getSubDirs()) {
+                q.offer(f);
+            }
+        }
+        rval.add(folder.getId());
+        return rval;
     }
 
     @Transactional
-    public void patchUpdate(FolderPatchRequest folderPatchRequest, Long currentFolderId) {
-        Folder folder = folderRepository.findById(currentFolderId).orElseThrow(NoSuchElementException::new);
+    @CacheEvict(value = "folderCache", key = "#targetFolderId")
+    public void patchUpdate(FolderPatchRequest folderPatchRequest, Long targetFolderId) {
+        Folder folder = folderRepository.findById(targetFolderId).orElseThrow(NoSuchElementException::new);
+        Objects.requireNonNull(cacheManager.getCache("folderCache")).evict(folder.getParent().getId());
         if (folderPatchRequest.getNewName() != null) {
             folder.setFolderName(folderPatchRequest.getNewName());
         }
@@ -62,6 +100,7 @@ public class FolderService {
             folder.getParent().getSubDirs().remove(folder);
             Folder newParent = folderRepository.findById(folderPatchRequest.getNewParent())
                     .orElseThrow(NoSuchElementException::new);
+            Objects.requireNonNull(cacheManager.getCache("folderCache")).evict(newParent.getId());
             folder.setParent(newParent);
             newParent.getSubDirs().add(folder);
         }
