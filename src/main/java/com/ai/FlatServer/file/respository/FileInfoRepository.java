a701cc85281431e63bcad46e4adbfdfff9767444
koojawon <jawonkoo@naver.com>
2024-04-09 15:57:26 +0900

    -feat 파일 캐싱 추가/ 즐겨찾기 유저별 조회 기능

--- src/main/java/com/ai/FlatServer/file/respository/FileInfoRepository.java
+++ src/main/java/com/ai/FlatServer/file/respository/FileInfoRepository.java
@@ -1,17 +1,24 @@
 package com.ai.FlatServer.file.respository;
 
 import com.ai.FlatServer.file.respository.dao.FileInfo;
+import com.ai.FlatServer.user.repository.entity.User;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
 
     Optional<FileInfo> findByUid(String uid);
+
+    @NonNull
+    @Cacheable(value = "fileCache", key = "#id", condition = "")
+    Optional<FileInfo> findById(@NonNull Long id);
 
     List<FileInfo> findAllByFav(boolean bool);
 
@@ -23,4 +30,6 @@ public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
 
     @Query("select * from FileInfo f where f.parentFolderId in :folderIds")
     List<FileInfo> selectAllByParentFolderId(List<Long> folderIds);
+
+    List<FileInfo> findAllByFavAndOwner(boolean b, User user);
 }
