c8cb72f95ab857c0623fcb2a9de3f403e7a2dd2a
koojawon <jawonkoo@naver.com>
2024-04-09 15:56:00 +0900

    -feat 유저 db 캐시 적용

--- src/main/java/com/ai/FlatServer/user/repository/UserRepository.java
+++ src/main/java/com/ai/FlatServer/user/repository/UserRepository.java
@@ -3,13 +3,13 @@ package com.ai.FlatServer.user.repository;
 import com.ai.FlatServer.user.enums.SocialType;
 import com.ai.FlatServer.user.repository.entity.User;
 import java.util.Optional;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Cacheable(key = "userCache", value = "#email")
     Optional<User> findByEmail(String email);
-
-    Optional<User> findByNickname(String nickname);
 
     Optional<User> findByRefreshToken(String refreshToken);
 
