f0db73d29f09ef1c4f23c00b0c553d6e8c627b6c
koojawon <jawonkoo@naver.com>
2024-04-11 16:09:03 +0900

    -fix nonnull 지정

--- src/main/java/com/ai/FlatServer/security/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/ai/FlatServer/security/filter/JwtAuthenticationProcessingFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,7 +31,8 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
     private final GrantedAuthoritiesMapper grantedAuthoritiesMapper = new NullAuthoritiesMapper();
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
         if (request.getRequestURI().equals(NO_CHECK_URI)) {
             filterChain.doFilter(request, response);
