c880d446969617a4db9a856aee00a1a83513b8fa
koojawon <jawonkoo@naver.com>
2024-04-12 16:21:07 +0900

    -feat refreshtoken 저장 redis용 dto

--- src/main/java/com/ai/FlatServer/user/repository/entity/UserRefreshToken.java
+++ src/main/java/com/ai/FlatServer/user/repository/entity/UserRefreshToken.java
@@ -1,12 +1,14 @@
 package com.ai.FlatServer.user.repository.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @AllArgsConstructor
 @Data
+@Builder
 @RedisHash("userRefreshToken")
 public class UserRefreshToken {
 
