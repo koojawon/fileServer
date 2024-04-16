cb0f8f24f4ce704a8d913c8b2fbfc645bcf3f71a
koojawon <jawonkoo@naver.com>
2024-04-16 21:12:46 +0900

    -chore 미사용 기능 제거

--- src/main/java/com/ai/FlatServer/security/handler/jsonlogin/LoginFailureHandler.java
+++ src/main/java/com/ai/FlatServer/security/handler/jsonlogin/LoginFailureHandler.java
@@ -1,17 +1,18 @@
 package com.ai.FlatServer.security.handler.jsonlogin;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-                                        AuthenticationException exception) throws IOException, ServletException {
+                                        AuthenticationException exception) throws IOException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/plain;charset=UTF-8");
