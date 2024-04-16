2ec1da634effd4239cb94f568401d8807c809ce8
koojawon <jawonkoo@naver.com>
2024-04-16 21:06:13 +0900

    -feat 하위 폴더 정보용 클래스 생성

---
+++ src/main/java/com/ai/FlatServer/folder/dto/response/SubFolderInfo.java
@@ -0,0 +1,18 @@
+package com.ai.FlatServer.folder.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubFolderInfo {
+    private Long id;
+    private String folderName;
+    private Long parentId;
+    private LocalDateTime modDate;
+}
