bb634d0b8a1a71ef1c206a214934ae0ae1729e9c
koojawon <jawonkoo@naver.com>
2024-04-16 21:10:11 +0900

    -feat user 캐싱하지 않게 변경!

--- src/main/java/com/ai/FlatServer/user/repository/UserRepository.java
+++ src/main/java/com/ai/FlatServer/user/repository/UserRepository.java
@@ -3,12 +3,10 @@ package com.ai.FlatServer.user.repository;
 import com.ai.FlatServer.user.enums.SocialType;
 import com.ai.FlatServer.user.repository.entity.User;
 import java.util.Optional;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Cacheable(key = "userCache", value = "#email")
     Optional<User> findByEmail(String email);
 
     Optional<User> findByRefreshToken(String refreshToken);
