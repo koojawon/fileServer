51cf7b26241a7a4dfa35f454b4c547eef31a665f
koojawon <jawonkoo@naver.com>
2024-04-20 13:59:51 +0900

    -feat 플랫별 상이한 응답 처리용 추상 객체

---
+++ src/main/java/com/ai/FlatServer/security/dto/oauth2/OAuth2UserInfo.java
@@ -0,0 +1,13 @@
+package com.ai.FlatServer.security.dto.oauth2;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class OAuth2UserInfo {
+    protected final Map<String, Object> attributes;
+
+    public abstract String getId();
+
+    public abstract String getNickname();
+}
