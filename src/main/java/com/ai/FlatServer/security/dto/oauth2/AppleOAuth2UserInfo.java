39522e9ee34cad3f13cf34bf6c8b4cbfdc7db2ef
koojawon <jawonkoo@naver.com>
2024-04-20 13:57:49 +0900

    -feat 각 플랫폼별 userinfo 구현

---
+++ src/main/java/com/ai/FlatServer/security/dto/oauth2/AppleOAuth2UserInfo.java
@@ -0,0 +1,27 @@
+package com.ai.FlatServer.security.dto.oauth2;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.util.Map;
+
+public class AppleOAuth2UserInfo extends OAuth2UserInfo {
+
+    public AppleOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson((String) attributes.get("user"), JsonObject.class);
+        return jsonObject.get("email").getAsString();
+    }
+
+    @Override
+    public String getNickname() {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson((String) attributes.get("user"), JsonObject.class);
+        JsonObject nameObject = jsonObject.getAsJsonObject("name");
+        return nameObject.get("firstName").getAsString() + nameObject.get("lastName").getAsString();
+    }
+}
---
+++ src/main/java/com/ai/FlatServer/security/dto/oauth2/XOAuth2UserInfo.java
@@ -0,0 +1,20 @@
+package com.ai.FlatServer.security.dto.oauth2;
+
+import java.util.Map;
+
+public class XOAuth2UserInfo extends OAuth2UserInfo {
+
+    public XOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+
+    @Override
+    public String getNickname() {
+        return "";
+    }
+}
