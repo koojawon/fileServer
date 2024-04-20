9ad204507184129f7514f59b74f92f75aa44f352
koojawon <jawonkoo@naver.com>
2024-04-20 14:03:35 +0900

    -chore 사용되지 않는 의존 제거

--- src/main/java/com/ai/FlatServer/security/handler/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/ai/FlatServer/security/handler/oauth2/OAuth2SuccessHandler.java
@@ -2,10 +2,8 @@ package com.ai.FlatServer.security.handler.oauth2;
 
 import com.ai.FlatServer.security.dto.oauth2.CustomOAuth2User;
 import com.ai.FlatServer.security.service.JwtService;
-import com.ai.FlatServer.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -17,11 +15,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final JwtService jwtService;
-    private final UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException {
+                                        Authentication authentication) {
         CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         loginSuccess(request, response, customOAuth2User);
     }
