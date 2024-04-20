73427e6a8a1a245b9726692fa864cec55e90fb25
koojawon <jawonkoo@naver.com>
2024-04-20 14:00:47 +0900

    -feat CustomOAuth2UserService 구현

--- src/main/java/com/ai/FlatServer/security/service/CustomOAuth2UserService.java
+++ src/main/java/com/ai/FlatServer/security/service/CustomOAuth2UserService.java
@@ -1,5 +1,16 @@
 package com.ai.FlatServer.security.service;
 
+import com.ai.FlatServer.security.dto.oauth2.CustomOAuth2User;
+import com.ai.FlatServer.security.factory.OAuthAttributes;
+import com.ai.FlatServer.user.enums.SocialType;
+import com.ai.FlatServer.user.repository.UserRepository;
+import com.ai.FlatServer.user.repository.entity.User;
+import jakarta.transaction.Transactional;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -7,12 +18,69 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private static final String APPLE = "apple";
+    private static final String FACEBOOK = "facebook";
+    private static final String X = "x";
+    private static final String GOOGLE = "google";
+    private final UserRepository userRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
-        return null;
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        SocialType socialType = getSocialType(registrationId);
+        if (socialType == null) {
+            throw new OAuth2AuthenticationException("Wrong URI");
+        }
+        String userNameAttributesName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        OAuthAttributes oAuthAttributes = OAuthAttributes.of(socialType, userNameAttributesName, attributes);
+        User createdUser = getUser(Objects.requireNonNull(oAuthAttributes), socialType);
+        return new CustomOAuth2User(Collections.singleton(new SimpleGrantedAuthority(createdUser.getRole().getKey())),
+                attributes, oAuthAttributes.getNameAttributeKey(), createdUser.getEmail(), createdUser.getRole());
+    }
+
+    private SocialType getSocialType(String registrationId) {
+        switch (registrationId) {
+            case X -> {
+                return SocialType.X;
+            }
+            case FACEBOOK -> {
+                return SocialType.FACEBOOK;
+            }
+            case APPLE -> {
+                return SocialType.APPLE;
+            }
+            case GOOGLE -> {
+                return SocialType.GOOGLE;
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    private User getUser(OAuthAttributes attributes, SocialType socialType) {
+        User findUser = userRepository.findBySocialTypeAndSocialId(socialType, attributes.getOAuth2UserInfo().getId())
+                .orElse(null);
+        if (findUser == null) {
+            return saveUser(attributes, socialType);
+        }
+        return findUser;
+    }
+
+    @Transactional
+    private User saveUser(OAuthAttributes attributes, SocialType socialType) {
+        User user = attributes.toEntity(socialType, attributes.getOAuth2UserInfo());
+        return userRepository.save(user);
     }
 }
