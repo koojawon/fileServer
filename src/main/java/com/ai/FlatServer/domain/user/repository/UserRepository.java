2cface0be13ab5b778338e9e74d28aaca2b83afa
koojawon <jawonkoo@naver.com>
2024-04-22 11:16:06 +0900

    -feat 유저 캐싱 적용

--- src/main/java/com/ai/FlatServer/domain/user/repository/UserRepository.java
+++ src/main/java/com/ai/FlatServer/domain/user/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.ai.FlatServer.domain.user.repository;
 import com.ai.FlatServer.domain.user.enums.SocialType;
 import com.ai.FlatServer.domain.user.repository.entity.User;
 import java.util.Optional;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Cacheable(cacheNames = "userCache", key = "'userInfo'+#email")
     Optional<User> findByEmail(String email);
 
     Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
