64391c36919620f61d85f23b17f43e7102b39e00
koojawon <jawonkoo@naver.com>
2024-04-25 16:42:31 +0900

    -refactor redis 비밀번호 설정 추가

--- src/main/java/com/ai/FlatServer/global/redis/config/RedisConfig.java
+++ src/main/java/com/ai/FlatServer/global/redis/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -18,7 +19,11 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setPassword(redisProperties.getPassword());
+        redisConfiguration.setPort(redisConfiguration.getPort());
+        redisConfiguration.setHostName(redisProperties.getHost());
+        return new LettuceConnectionFactory(redisConfiguration);
     }
 
     @Bean
