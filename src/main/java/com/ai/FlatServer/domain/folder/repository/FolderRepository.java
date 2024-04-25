926cdca7bcf4aca04d8dc80cfb510a83fb7fea3d
koojawon <jawonkoo@naver.com>
2024-04-25 16:41:04 +0900

    -refactor delete 메소드에 transactional 지정, 이중 캐시 문제 해결

--- src/main/java/com/ai/FlatServer/domain/folder/repository/FolderRepository.java
+++ src/main/java/com/ai/FlatServer/domain/folder/repository/FolderRepository.java
@@ -1,12 +1,13 @@
 package com.ai.FlatServer.domain.folder.repository;
 
 import com.ai.FlatServer.domain.folder.repository.entity.Folder;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
@@ -15,10 +16,10 @@ public interface FolderRepository extends JpaRepository<Folder, Long> {
 
     @Modifying
     @Query("delete from Folder f where f.id in :ids")
-    void deleteAllByIds(List<Long> ids);
+    @Transactional
+    void deleteAllByIds(@Param("ids") List<Long> ids);
 
     @NonNull
-    @Cacheable(cacheNames = "folderCache", key = "#id")
     Optional<Folder> findById(@NonNull Long id);
 
     List<Folder> findByParentId(Long id);
