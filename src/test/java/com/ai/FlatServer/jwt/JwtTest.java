534be2753faf167965222efcd8029331fa68d68b
koojawon <jawonkoo@naver.com>
2024-05-04 12:55:01 +0900

    -feat jwt 관련 테스트 작성

---
+++ src/test/java/com/ai/FlatServer/jwt/JwtTest.java
@@ -0,0 +1,7 @@
+package com.ai.FlatServer.jwt;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class JwtTest {
+}
