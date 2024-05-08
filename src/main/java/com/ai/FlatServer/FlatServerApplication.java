d64351963c6e0be726dba0d89c290b5b5cfd1010
koojawon <jawonkoo@naver.com>
2024-05-08 09:41:35 +0900

    -fix kurento 남은 의존성 제거

--- src/main/java/com/ai/FlatServer/FlatServerApplication.java
+++ src/main/java/com/ai/FlatServer/FlatServerApplication.java
@@ -1,16 +1,11 @@
 package com.ai.FlatServer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.kurento.client.KurentoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @SpringBootApplication
-@EnableWebSocket
 @EnableJpaAuditing
 @Slf4j
 public class FlatServerApplication {
@@ -19,15 +14,4 @@ public class FlatServerApplication {
         SpringApplication.run(FlatServerApplication.class, args);
     }
 
-    @Bean
-    public KurentoClient kurentoClient() {
-        return KurentoClient.create();
-    }
-
-    @Bean
-    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxTextMessageBufferSize(32768);
-        return container;
-    }
 }
